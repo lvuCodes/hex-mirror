0c932e02c0ad2e7cc1b84a394a11931d44a911ba
L[auren] Vu <lehvu95@gmail.com>
2026-04-05 19:48:13 +0000

    styling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,31 @@
+import styled from "styled-components";
+
+const StyledInput = styled.div`
+  width: 90%;
+  border-radius: 10px;
+  margin: 20px auto;
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  font-size: 24px;
+
+  input {
+    max-width: 80%;
+    border: none;
+    border-bottom: 2px solid black;
+    font-family: monospace;
+    font-size: 24px;
+    text-align: center;
+    margin: auto;
+  }
+`;
+
 const Input = () => {
   return (
-    <>
-      <label>Hexcode: #</label>
+    <StyledInput>
       <input type="text" id="hex-input" name="hex-input" placeholder="000000" />
-    </>
+    </StyledInput>
   );
 };
-
-{
-  /* <form action="/action_page.php">
-  <label for="fname">First name:</label>
-  <input type="text" id="fname" name="fname"><br><br>
-  <label for="lname">Last name:</label>
-  <input type="text" id="lname" name="lname"><br><br>
-  <input type="submit" value="Submit">
-</form> */
-}
 
 export default Input;
