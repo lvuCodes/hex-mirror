e1ece6ce2e2ea90b9c408745ee5ea39f4f94474a
lehvu95 <lehvu95@gmail.com>
2026-04-05 18:00:33 +0000

    updates

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,14 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "./store";
 import { AppDispatch } from "./store";
-import { selectHexCode, setHexCode } from "./slices";
+// import { selectHexCode, setHexCode } from "./slices";
 
-export function useColor() {
-  const dispatch = useDispatch<AppDispatch>();
-  const hexCode = useSelector((state: RootState) => state.color.hexCode);
+// export function useColor() {
+//   const dispatch = useDispatch<AppDispatch>();
+//   const hexCode = useSelector((state: RootState) => state.color.hexCode);
 
-  return {
-    hexCode,
-    setHexCode: (value: string) => dispatch(setHexCode(value)),
-  };
-}
+//   return {
+//     hexCode,
+//     setHexCode: (value: string) => dispatch(setHexCode(value)),
+//   };
+// }
