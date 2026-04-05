0b6a7d3578c8a3c8bc178fb1204548fc2a04b3e0
L[auren] Vu <lehvu95@gmail.com>
2026-04-05 17:18:56 +0000

    regex util

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./styles.css";
+import Input from "./components/Input";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hex Mirror</h1>
+      <h2>Provide a hexcode to find its mirror hex.</h2>
+      <Input />
     </div>
   );
 }
