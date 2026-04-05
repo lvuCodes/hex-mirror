7d3143426887478182ba7ee2d4a3a115ac143599
L[auren] Vu <lehvu95@gmail.com>
2026-04-05 17:34:47 +0000

    store

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,107 +1,14 @@
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { useNavigate, useLocation } from "react-router-dom";
-// import { AsyncThunk } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "./use-app-selector";
+import { AppDispatch } from "./store";
+import { selectHexCode, setHexCode } from "./slices";
 
-// import { AppDispatch } from "../store";
-// import { useAppSelector } from "./use-app-selector";
+export function useColor() {
+  const dispatch = useDispatch<AppDispatch>();
+  const hexCode = useAppSelector(selectHexCode);
 
-// // Import selectors and actions from the relevant slice
-// // import { selectExampleValue, selectIsLoading } from '../slices/template-slice';
-// // import { someAction, someAsyncThunk } from '../slices/template-slice';
-
-// // ------------------------------------------------------------
-// // 1. Selector-only hook
-// //    Read state; no dispatch.
-// // ------------------------------------------------------------
-// export function useTemplateData(): {
-//   exampleValue: string | null;
-//   isLoading: boolean;
-// } {
-//   const exampleValue = useAppSelector((state) => state.template.exampleValue);
-//   const isLoading = useAppSelector((state) => state.template.isLoading);
-//   // Prefer imported createSelector selectors over inline lambdas:
-//   // const exampleValue = useAppSelector(selectExampleValue);
-
-//   return {
-//     exampleValue,
-//     isLoading,
-//   };
-// }
-
-// // ------------------------------------------------------------
-// // 2. Dispatch hook
-// //    Write state; wraps one or more actions.
-// // ------------------------------------------------------------
-// export function useTemplateActions(): {
-//   handleSomeAction: (value: string) => void;
-// } {
-//   const dispatch = useDispatch<AppDispatch>();
-
-//   const handleSomeAction = (value: string): void => {
-//     // dispatch(someAction(value));
-//   };
-
-//   return {
-//     handleSomeAction,
-//   };
-// }
-
-// // ------------------------------------------------------------
-// // 3. Combined read/write hook
-// //    Selector + dispatch in one hook when tightly coupled.
-// // ------------------------------------------------------------
-// export function useTemplate(): {
-//   exampleValue: string | null;
-//   isLoading: boolean;
-//   handleSomeAction: (value: string) => void;
-// } {
-//   const dispatch = useDispatch<AppDispatch>();
-//   const exampleValue = useAppSelector((state) => state.template.exampleValue);
-//   const isLoading = useAppSelector((state) => state.template.isLoading);
-
-//   const handleSomeAction = (value: string): void => {
-//     // dispatch(someAction(value));
-//   };
-
-//   return {
-//     exampleValue,
-//     isLoading,
-//     handleSomeAction,
-//   };
-// }
-
-// // ------------------------------------------------------------
-// // 4. Async loader hook
-// //    Mirrors useAsyncLoader pattern in use-async-loader.ts.
-// //    Dispatches a thunk on mount; handles retry and cleanup.
-// // ------------------------------------------------------------
-// export function useTemplateLoader(): void {
-//   const dispatch = useDispatch<AppDispatch>();
-
-//   useEffect(() => {
-//     const promise = dispatch(/* someAsyncThunk() */ null);
-
-//     return () => {
-//       promise?.abort?.();
-//     };
-//   }, [dispatch]);
-// }
-
-// // ------------------------------------------------------------
-// // 5. Router hook
-// //    Mirrors useGoToRoute pattern in use-goto-route.ts.
-// // ------------------------------------------------------------
-// // import { routes } from '../utils/helpers/routing-helpers';
-// //
-// // export function useTemplateNavigation(): {
-// //     goToTemplate: () => void;
-// // } {
-// //     const navigate = useNavigate();
-// //
-// //     const goToTemplate = (): void => {
-// //         navigate(routes.TEMPLATE_ROUTE);
-// //     };
-// //
-// //     return { goToTemplate };
-// // }
+  return {
+    hexCode,
+    setHexCode: (value: string) => dispatch(setHexCode(value)),
+  };
+}
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,24 @@ export const isHexCode = (input: string) => {
   const hexRegex: RegExp = /^([0-9A-Fa-f]{3}){1,2}$/;
   return hexRegex.test(input);
 };
+
+const decToHex = (num: number): String => {
+  return num.toString(16).padStart(2, "0").toUpperCase();
+};
+
+export const getRandom256 = (): number => {
+  return Math.floor(Math.random() * 255);
+};
+
+export const randomDecToHex = (): String => {
+  return decToHex(getRandom256());
+};
+
+// #RRGGBB
+export const getRandomHex = () => {
+  const R = randomDecToHex();
+  const G = randomDecToHex();
+  const B = randomDecToHex();
+
+  return `${R}${G}${B}`;
+};
