01d9c61ab8ee24351de588382839a255ece4aa43
lehvu95 <lehvu95@gmail.com>
2026-04-06 18:21:28 +0000

    mirror sets

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import "./styles.css";
 import DisplayCard from "./components/DisplayCard";
-import InputCard from "./components/InputCard";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hex Mirror</h1>
       <h2>Provide a hexcode to find its mirror hex.</h2>
-      <InputCard />
+      <DisplayCard />
     </div>
   );
 }
