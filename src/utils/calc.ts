01019efdb85bbba1f697bd44600fa1ec20884abe
L[auren] Vu <lehvu95@gmail.com>
2026-04-14 18:52:53 -0500

    bug fix

--- src/utils/calc.ts
+++ src/utils/calc.ts
@@ -1,11 +1,11 @@
 export const arrayAvg = (arr: number[]): number =>
   arr.reduce((sum, val) => sum + val, 0) / arr.length;
 
-export const absPercentDiff = (a: number, b: number): number =>
-  Math.abs(absPercentDiff(a, b));
-
 export const percentDiff = (a: number, b: number): number =>
   (a - b) / ((a + b) / 2);
+
+export const absPercentDiff = (a: number, b: number): number =>
+  Math.abs(percentDiff(a, b));
 
 export const format = (n: number | undefined): string =>
   String(n ?? 0).slice(0, 6);
