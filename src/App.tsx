3fa01e3b02c82a9fa4ce069300c230140e09831f
lehvu95 <lehvu95@gmail.com>
2026-04-14 23:19:45 +0000

    average diff and percent diff

--- src/App.tsx
+++ src/App.tsx
@@ -19,3 +19,12 @@ export default function App() {
 // - opacity slider
 // - submit button
 // - hover state to indicate which mirror hex
+
+// Average Absolute Diff - Google Sheets dark vs light
+//              737373    757171    |diff|    % diff
+// Red          115       117         2       0.0172
+// Green        115       113         2       0.0175
+// Blue         115       113         2       0.0175
+// Hue          0         0.8802    0.8802    2
+// Saturation   0         0.0200    0.0200    2
+// Lightness    0.4509    0.4511    0.0001    0.0003
